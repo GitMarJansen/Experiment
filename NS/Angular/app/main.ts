44003932926805253eda7a874ac03a3eea9d616c
Hilmar Jansen <hilmar.jansen@planonsoftware.com>
2016-05-23 12:50:25 +0200

    App start performance - no success

--- NS/Angular/app/main.ts
+++ NS/Angular/app/main.ts
@@ -1,4 +1,6 @@
 import {nativeScriptBootstrap} from "nativescript-angular/application";
 import {AppComponent} from "./app.component";
+import {enableProdMode} from "@angular/core";
+enableProdMode();
 
 nativeScriptBootstrap(AppComponent);
